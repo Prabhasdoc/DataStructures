244f6e84f0d4b0bba8ba18ec9ea703b60cab9cc3
prabhasdoc <teluguprabhas76@gmail.com>
2024-09-18 11:26:53 +0530

    first commit

--- LanguageBasics/Patterns/SolidRectangle.java
+++ LanguageBasics/Patterns/SolidRectangle.java
@@ -6,8 +6,8 @@ public class SolidRectangle {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
-            for (int i = 0; i <= n; i++) {
-                for (int j = 0; j <= n; j++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     System.out.print("* ");
 
                 }
