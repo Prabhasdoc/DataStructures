9dd496af6f697ffe648f290eefe5fc6823af2bbf
prabhasdoc <teluguprabhas76@gmail.com>
2024-09-18 11:24:01 +0530

    programfile

---
+++ LanguageBasics/Patterns/SolidRectangle.java
@@ -0,0 +1,20 @@
+package DataStructures.LanguageBasics.Patterns;
+
+import java.util.Scanner;
+
+public class SolidRectangle {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j <= n; j++) {
+                    System.out.print("* ");
+
+                }
+                System.out.println();
+
+            }
+        }
+
+    }
+}
