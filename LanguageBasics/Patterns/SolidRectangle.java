f22a8b8268c9cb42d9c7bdf653c6a34731b85efb
Prabhas Telugu <102602837+Prabhasdoc@users.noreply.github.com>
2024-09-19 22:04:34 +0530

    Update SolidRectangle.java

--- LanguageBasics/Patterns/SolidRectangle.java
+++ LanguageBasics/Patterns/SolidRectangle.java
@@ -6,7 +6,7 @@ public class SolidRectangle {
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
-            // Read an integer from the user, which represents the size of the rectangle
+
             int n = sc.nextInt();
 
             // Loop through each row of the rectangle
